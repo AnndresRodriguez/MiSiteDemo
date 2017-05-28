f8832e7b490447a2ee7e9419ebe87d2b37a9915a
Andres <andresjoe24@gmail.com>
2017-05-28 13:25:08 -0500

    corregido los media queries y minificado css y js

--- js/animations-circle.js
+++ js/animations-circle.js
@@ -45,7 +45,7 @@ $('[data-toggle="tooltip"]').tooltip();
 function animateSkills() {
 
   $('.circle-html5').circleProgress({ value: 0.5,fill:{color:'#F46135'},});
-  $('.circle-css3').circleProgress({ value: 0.6, fill:{color:'#32A3DB'} });
+  $('.circle-css3').circleProgress({ value: 0.65, fill:{color:'#32A3DB'} });
   $('.circle-js').circleProgress({ value: 0.5, fill:{color:'#FED93A'} });
   $('.circle-jquery').circleProgress({ value: 0.6, fill:{color:'#046AAE'} });
   $('.circle-bootstrap').circleProgress({ value: 0.5, fill:{color:'#5E4388'} });
@@ -53,8 +53,9 @@ function animateSkills() {
   $('.circle-php').circleProgress({ value: 0.35, fill:{color:'#5969A6'} });
   $('.circle-laravel').circleProgress({ value: 0.25, fill:{color:'#F76661'} });
   $('.circle-mysql').circleProgress({ value: 0.25, fill:{gradient: [["#01678B", .5], ["#E59203", .5]] }});
-  $('.circle-git').circleProgress({ value: 0.5, fill:{color:'#FC511F'} });
-  $('.circle-github').circleProgress({ value: 0.5, fill:{color:'#000'} });
+  $('.circle-git').circleProgress({ value: 0.4, fill:{color:'#FC511F'} });
+  $('.circle-github').circleProgress({ value: 0.4, fill:{color:'#000'} });
+
 }
 
 
